625c25f54777737104913e3dc7ea67c552747c64
kumdo0327 <kumdo0327@naver.com>
2025-05-07 16:09:51 +0900

    Update

--- include/DiffInk/Api.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIFFINK_API_H
-#define DIFFINK_API_H
-
-#include "DiffInk/AST/MerkleTree.h"
-#include "DiffInk/TreeDiff/CompositeMatcher.h"
-#include "DiffInk/TreeDiff/TreeDiff.h"
-#include "DiffInk/Utils/SmartParser.h"
-
-#endif // DIFFINK_API_H
